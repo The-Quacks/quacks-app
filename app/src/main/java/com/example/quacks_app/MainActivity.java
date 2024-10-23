26385187ab79cf73c9e73dc278836370cfe4a8e4
Aiden <arpeacoc@ualberta.ca>
2024-10-22 18:45:06 -0600

    AdminEntryPage

--- app/src/main/java/com/example/quacks_app/MainActivity.java
+++ app/src/main/java/com/example/quacks_app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quacks_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.admin_home);
+
+        ImageButton Profile = findViewById(R.id.profilesButton);
+
+        Profile.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "PROFILE!", Toast.LENGTH_SHORT).show();
         });
+
+
     }
 }
