cd45b0beccacb7fa3218a3a5a145eddfe0ddabe8
Nicholas Weiss <nsweiss@ualberta.ca>
2024-10-30 15:59:43 -0600

    Making sure tests also fail in actions

--- app/src/test/java/com/example/quacks_app/ExampleUnitTest.java
+++ app/src/test/java/com/example/quacks_app/ExampleUnitTest.java
@@ -9,4 +9,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void fail_test() {
+        assertEquals(4, 5);
+    }
 }
