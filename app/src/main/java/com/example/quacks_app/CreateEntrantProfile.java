154adfd711357081fcf4370da06db956e3833a71
Nicholas Weiss <nsweiss@ualberta.ca>
2024-11-05 15:38:18 -0700

    Added ability to create new user profile

--- app/src/main/java/com/example/quacks_app/CreateEntrantProfile.java
+++ app/src/main/java/com/example/quacks_app/CreateEntrantProfile.java
@@ -3,10 +3,15 @@ package com.example.quacks_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.telephony.PhoneNumberUtils;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class CreateEntrantProfile extends AppCompatActivity {
     @Override
@@ -27,18 +32,45 @@ public class CreateEntrantProfile extends AppCompatActivity {
             EditText email = findViewById(R.id.emailInput);
             EditText phoneNumber = findViewById(R.id.editTextPhone);
 
-            UserProfile newProfile = new UserProfile();
-            newProfile.setUserName(name.getText().toString());
-            newProfile.setEmail(email.getText().toString());
-            newProfile.setPhoneNumber(phoneNumber.getText().toString());
+            if (name.getText().toString().isEmpty()) {
+                Toast nameToast = Toast.makeText(this, "Please enter a valid name", Toast.LENGTH_SHORT);
+                nameToast.show();
+                return;
+            }
+
+            if (email.getText().toString().isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()) {
+                Toast emailToast = Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT);
+                emailToast.show();
+                return;
+            }
+
+            int phoneNumberLength = phoneNumber.getText().toString().length();
+            if (!phoneNumber.getText().toString().isEmpty() && phoneNumberLength != 10) {
+                Toast phoneToast = Toast.makeText(this, "Please enter a valid phone number", Toast.LENGTH_SHORT);
+                phoneToast.show();
+                return;
+            }
+
+            UserProfile newProfile = new UserProfile(name.getText().toString(), email.getText().toString(), phoneNumber.getText().toString());
 
             String mId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-//            User newUser = new User(mId);
-//            newUser.setUserProfile(newProfile);
-//
-//            CRUD<User> crud = new CRUD<>(User.class);
-//            crud.create(newUser);
-//            startActivity(home);
+            ArrayList<Role> roles = new ArrayList<>();
+            roles.add(Role.ENTRANT);
+            User newUser = new User(mId, roles, newProfile);
+
+            CRUD<User> crud = new CRUD<>(User.class);
+            CreateCallback createCallback = new CreateCallback() {
+                @Override
+                public void onCreateSuccess() {
+                    startActivity(home);
+                }
+
+                @Override
+                public void onCreateFailure(Exception e) {
+                    Toast.makeText(CreateEntrantProfile.this, "Error creating user, please try again", Toast.LENGTH_SHORT).show();
+                }
+            };
+            crud.create(newUser, createCallback);
         });
     }
 }
--- app/src/main/java/com/example/quacks_app/EntrantHome.java
+++ app/src/main/java/com/example/quacks_app/EntrantHome.java
@@ -2,10 +2,16 @@ package com.example.quacks_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntrantHome extends AppCompatActivity {
     @Override
@@ -19,9 +25,36 @@ public class EntrantHome extends AppCompatActivity {
         ImageButton notifications = findViewById(R.id.notificationsButton);
         ImageButton scanQRCode = findViewById(R.id.scanQRCodeButton);
 
+        ReadMultipleCallback<User> readMultipleCallback = new ReadMultipleCallback<User>() {
+            @Override
+            public void onReadMultipleSuccess(ArrayList<User> data) {
+                ImageButton profile = findViewById(R.id.profileButton);
+                String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                for (User user : data) {
+                    if (user.getDeviceId().equals(deviceId)) {
+                        profile.setTag("true");
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onReadMultipleFailure(Exception e) {
+                Toast.makeText(EntrantHome.this, "Could not connect to database", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Map<String, String> deviceId = new HashMap<>();
+        deviceId.put("deviceId", Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
+        CRUD<User> crud = new CRUD<>(User.class);
+        crud.readQueryStatic(deviceId, readMultipleCallback);
+
         profile.setOnClickListener(view -> {
-            if (true) { // If user doesn't have a profile
+            if (profile.getTag().equals("false")) {
                 startActivity(new Intent(EntrantHome.this, CreateEntrantProfile.class));
+            }
+            else {
+                Toast.makeText(EntrantHome.this, "Go to profile activity", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(EntrantHome.this, EntrantProfile.class));
             }
         });
 
