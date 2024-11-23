00040992d54eb979878779aadd813e3e1e068c91
Josie Paananen <jpaanane@ualberta.ca>
2024-11-22 17:18:55 -0700

    changes with main

--- app/src/main/java/com/example/quacks_app/EditEvent.java
+++ app/src/main/java/com/example/quacks_app/EditEvent.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -23,13 +24,13 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class EditEvent extends AppCompatActivity {
+    private CheckBox geolocation;
     private LocalDate start_date;
     private EditText event_name;
     private EditText class_capacity;
     private EditText waitlist_capacity;
     private EditText beginning;
     private EditText instructor;
-    private EditText geolocation;
     private EditText description;
     private Facility facility;
     private User user;
@@ -39,20 +40,17 @@ public class EditEvent extends AppCompatActivity {
     private ImageButton search;
     private ImageButton profile;
     private ImageButton homepage;
-    private int test_one = 0;
-    private int test_two = 0;
-    private int test_three = 0;
-    private int test_five = 0;
-    private int test_six = 0;
-    private int test_seven = 0;
-    private int test_eight = 0;
-    private int wrong = 0;
+    private boolean wrong = false;
     private FirebaseFirestore db;
     private EditText eventtime;
     private Date final_date_time;
     private Event old_event;
     private int classes = 0;
     private int classes_two = 0;
+    private boolean validDate;
+    private boolean validInstructorName;
+    private boolean validEventName;
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -96,8 +94,9 @@ public class EditEvent extends AppCompatActivity {
         class_capacity.setText(String.valueOf(old_event.getRegistrationCapacity()));
         waitlist_capacity.setText(String.valueOf(old_event.getWaitlistCapacity()));
         instructor.setText(old_event.getInstructor());
-        geolocation.setText(old_event.getGeo());
         description.setText(old_event.getDescription());
+        geolocation.setChecked(old_event.getGeolocationRequired());
+
 
         //getting the hour
         Date date = old_event.getDateTime();
@@ -150,8 +149,6 @@ public class EditEvent extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //this checks the format of the **day** entered
                 try{
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
                     String startdate = beginning.getText().toString().trim();
@@ -161,9 +158,11 @@ public class EditEvent extends AppCompatActivity {
 
 
                     if (current_date.isBefore(start_date)){
-                        test_one = 1;
+                        validDate = true;
+
+
                     }
-                } catch (Exception e){
+                } catch (Exception e) {
                     Toast.makeText(EditEvent.this, "Format Dates dd-mm-yyyy", Toast.LENGTH_SHORT).show();
                 }
 
@@ -178,14 +177,15 @@ public class EditEvent extends AppCompatActivity {
                     try {
                         eventTime = LocalTime.parse(hour.toUpperCase(), formatted);
                     } catch (Exception E) {
-                        wrong = 1;
+                        wrong = true;
                         Toast.makeText(EditEvent.this, "Format Event Time 4:00pm", Toast.LENGTH_SHORT).show();
                     }
 
-                    if (wrong == 1) {
-                        wrong = 0;
+                    if (wrong) {
+                        wrong = false;
+                        validDate = false;
                     } else {
-                        test_two = 1;
+                        validDate = true;
                     }
 
                     if (start_date != null && eventTime != null) {
@@ -199,82 +199,60 @@ public class EditEvent extends AppCompatActivity {
                         Toast.makeText(EditEvent.this, "Invalid date or time input.", Toast.LENGTH_SHORT).show();
                     }
                 } catch(Exception E){
-                    test_two = 0;
+                    validDate = false;
                 }
 
 
                 // this checks the format of the **class capacity**
 
+                int classes = 0;
                 try {
                     classes = Integer.parseInt(class_capacity.getText().toString());
                 } catch(Exception e){
-                    wrong = 1;
+                    wrong = true;
                     Toast.makeText(EditEvent.this, "Error in Format for Class Capacity", Toast.LENGTH_SHORT).show();
                 }
 
-                if (wrong == 1){
-                    wrong = 0;
-                }else{
-                    test_three = 1;
-                    //Toast.makeText(CreateEvent.this, "3 passed", Toast.LENGTH_SHORT).show();
+                if (wrong) {
+                    wrong = false;
                 }
-
-                //this checks the format of the **waitlist capacity**
+                int classes_two = 0;
                 try {
                     classes_two = Integer.parseInt(waitlist_capacity.getText().toString());
-
-                }catch(Exception e){
-                    wrong = 1;
+                } catch(Exception e) {
+                    wrong = true;
                     Toast.makeText(EditEvent.this, "Error in Format for Waitlist Capacity", Toast.LENGTH_SHORT).show();
                 }
 
-                if (wrong != 1){
-                    test_five = 1;
-                    //Toast.makeText(CreateEvent.this, "5 passed", Toast.LENGTH_SHORT).show();
-                }
 
-                // this checks the format of the **instructor name**
+
                 String name = instructor.getText().toString();
-                if (name.length()>=1 && name.length() <= 40 ){
-                    test_six = 1;
+                if (name.length() >= 1 && name.length() <= 40 ) {
+                    validInstructorName = true;
                     //Toast.makeText(CreateEvent.this, "6 passed", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(EditEvent.this, "Error Instructor length needs to be between 1-40 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EditEvent.this, "Error: Instructor name needs to be between 1-40 characters", Toast.LENGTH_SHORT).show();
                 }
 
-                // this checks the format of the **Geolocation**
+                boolean geo = geolocation.isChecked();
 
-                String geo = geolocation.getText().toString();
-                if (geo.contains("Disabled")){
-                    test_seven = 1;
-                    //Toast.makeText(CreateEvent.this, "7 passed", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(EditEvent.this, "Geolocation has two options: Enabled/Disabled", Toast.LENGTH_SHORT).show();
-                }
-                if (geo.contains("Enabled")){
-                    Toast.makeText(EditEvent.this, "Enabled geolocation is not currently available.", Toast.LENGTH_SHORT).show();
-                }
-
-                // this checks the format of the **event name**
 
                 String eventname = event_name.getText().toString();
-                if (eventname.length() <= 40){
-                    test_eight = 1;
+                if (eventname.length() <= 40) {
+                    validEventName = true;
                     //Toast.makeText(CreateEvent.this, "8 passed", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(EditEvent.this, "Event Name needs to be less than 40 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EditEvent.this, "Event name needs to be less than 40 characters", Toast.LENGTH_SHORT).show();
                 }
                 String text = description.getText().toString();
 
-
-                //this is the case where all the tests pass
-                if (test_one == 1 && test_two == 1 && test_three == 1 && test_five == 1 && test_six == 1 && test_seven == 1&& test_eight == 1 ) {
+                if (!wrong && validDate && validInstructorName && validEventName) {
 
                     old_event.setEventName(eventname);
                     old_event.setDateTime(final_date_time);
                     old_event.setDescription(text);
                     old_event.setInstructor(name);
-                    old_event.setGeo(geo);
+                    old_event.setGeolocationRequired(geo);
                     old_event.setOrganizerId(user.getDocumentId());
                     old_event.setFacility(facility.getDocumentId());
                     old_event.setRegistrationCapacity(classes);
