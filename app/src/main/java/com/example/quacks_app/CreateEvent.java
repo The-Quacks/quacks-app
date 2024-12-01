1d906cf0734cd87b0636d760d63aedc5b6ce8ec1
Aiden <arpeacoc@ualberta.ca>
2024-12-01 12:44:45 -0700

    conflict fixing

--- app/src/main/java/com/example/quacks_app/CreateEvent.java
+++ app/src/main/java/com/example/quacks_app/CreateEvent.java
@@ -218,10 +218,6 @@ public class CreateEvent extends AppCompatActivity {
                     notificationList.setNotificationListId(notificationListId);
                     event.setNotificationList(notificationList);
 
-
-                    String uuid = UUID.randomUUID().toString();
-                    event.setDocumentId(uuid);
-
                     notificationList.setNotificationEventId(event.getEventId());
 
                     //Toast.makeText(CreateEvent.this, "It reaches the bottom", Toast.LENGTH_SHORT).show();
@@ -272,45 +268,6 @@ public class CreateEvent extends AppCompatActivity {
                     Toast.makeText(CreateEvent.this, "Validation Failed. Please Try Again", Toast.LENGTH_SHORT).show();
                 }
 
-            }
-        });
-
-        //This is the bottom of the page directory
-        homepage = findViewById(R.id.house);
-        profile = findViewById(R.id.person);
-        search = findViewById(R.id.search);
-
-        homepage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Already here
-                Intent intent = new Intent(CreateEvent.this, OrganizerHomepage.class);
-                if (facility != null) {
-                    intent.putExtra("Facility", facility);
-                }
-                startActivity(intent);
-            }
-        });
-
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CreateEvent.this, ViewOrganizer.class);
-                if (facility != null) {
-                    intent.putExtra("Facility", facility);
-                }
-                startActivity(intent);
-            }
-        });
-
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CreateEvent.this, ViewOrganizer.class);
-                if (facility != null) {
-                    intent.putExtra("Facility", facility);
-                }
-                startActivity(intent);
             }
         });
     }
